4288cc8c176d719885168e56185ce3c1d382deb6
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-06-11 15:18:37 -0300

    v.001

---
+++ jdbc/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import db.DB;
+
+import java.sql.Connection;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
