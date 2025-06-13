47d3403c7daffa8b7e29dd49716be5c01911d1f0
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-06-13 16:25:08 -0300

    v.006

--- jdbc/src/main/java/aplicacao/Programa.java
+++ jdbc/src/main/java/aplicacao/Programa.java
@@ -2,37 +2,48 @@ package aplicacao;
 
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Programa {
 
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st = null;
+        Statement st = null;
         try {
             conn = DB.getConnection();
 
-            st = conn.prepareStatement(
-                    "DELETE FROM department "
-                    + "WHERE "
-                    + "Id = ?"
-            );
+            conn.setAutoCommit(false);
 
-            st.setDouble(1, 200);;
+            st = conn.createStatement();
 
-            int linhasAfetadas = st.executeUpdate();
+            int linha1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
-            System.out.println("Pronto! Linhas Afetadas: " + linhasAfetadas);
-        }
-        catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
-        }
-        finally {
+//            int x = 1;
+//            if (x < 2) {
+//                throw new SQLException("Fake error");
+//            }
+
+            int linha2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("linha 1 " + linha1);
+            System.out.println("linha 2 " + linha2);
+
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+                throw new DbException("Transação nao ocorreu! Por causa de: " + e.getMessage());
+            }
+            catch (SQLException e1) {
+                throw new DbException("Error ao tentar retornar transação! Por causa de: " + e1.getMessage());
+            }
+        } finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
