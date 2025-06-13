0ad09c5410de902d3962039119565f0f2f5f8dbb
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-06-13 15:39:07 -0300

    v.004

--- jdbc/src/main/java/aplicacao/Programa.java
+++ jdbc/src/main/java/aplicacao/Programa.java
@@ -3,55 +3,41 @@ package aplicacao;
 
 import db.DB;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Programa {
 
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                    + "VALUES "
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)"
+            );
 
-            st.setString(1, "Lavinia Venturim");
-            st.setString(2,"lavinia.venturim@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("21/06/2008").getTime()));
-            st.setDouble(4, 3000);
-            st.setInt(5, 2);
+            st.setDouble(1, 200);;
+            st.setInt(2, 2);
 
-            int linhaAltereda = st.executeUpdate();
+            int linhasAfetadas = st.executeUpdate();
 
-            if (linhaAltereda > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Pronto! ID = " + id);
-                }
-            }
-            else {
-                System.out.println("Nenhuma Linha Alterada!");
-            }
+            System.out.println("Pronto! Linhas Afetadas: " + linhasAfetadas);
         }
         catch (SQLException e) {
-            e.printStackTrace();
-        }
-        catch (ParseException e) {
             e.printStackTrace();
         }
         finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
+
+
     }
 }
